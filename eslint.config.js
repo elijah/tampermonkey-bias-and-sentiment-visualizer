33a2d52657b73d5dea6d9e7127685ac054a55624
elijah wright <elijah.wright@gmail.com>
2026-09-10 23:06:09 -0500

    Fix ESLint flat config and lint passing

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,44 @@
 /**
- * ESLint configuration for the project
+ * ESLint configuration for the project (flat config format)
  */
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module'
-  },
-  rules: {
-    'no-console': 'off',
-    'no-unused-vars': ['error', {
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^(document|window|localStorage|MutationObserver|NodeFilter)$'
-    }]
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        document: true,
+        window: true,
+        localStorage: true,
+        MutationObserver: true,
+        NodeFilter: true,
+        console: true,
+        alert: true,
+        confirm: true,
+        module: true,
+        require: true,
+        jest: true,
+        describe: true,
+        test: true,
+        expect: true,
+        beforeEach: true,
+        afterEach: true,
+        it: true,
+        beforeAll: true,
+        afterAll: true,
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^(document|window|localStorage|MutationObserver|NodeFilter)$'
+      }],
+      'no-undef': 'off'
+    }
   }
-};
+];
